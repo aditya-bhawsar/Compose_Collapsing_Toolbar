1403c2703a96ea142d4a76009834faaa4d5788b1
AdityaBhawsar <aditya.bhawsar@mutualmobile.com>
2021-11-13 12:28:29 +0530

    Added A Extended FAB to main screen

--- app/src/main/java/com/aditya/composecollapsingtoolbar/MainActivity.kt
+++ app/src/main/java/com/aditya/composecollapsingtoolbar/MainActivity.kt
@@ -3,9 +3,12 @@ package com.aditya.composecollapsingtoolbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import com.aditya.composecollapsingtoolbar.ui.theme.ComposeCollapsingToolbarTheme
 
+@ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
